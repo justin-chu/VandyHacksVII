c98382686ed9213c795622da6c42296b41dc9717
rojan96 <rojanm874@gmail.com>
2020-10-04 02:07:41 -0500

    Completing tasks increases balance

--- frontend/src/pages/dashboard/dashboard.js
+++ frontend/src/pages/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./dashboard.css";
 import Coin from "../../images/coin.gif";
 import Avatar from "../../images/avatar.png";
@@ -7,127 +7,136 @@ import { getCustomerInfo } from "../../utils/backend";
 import TaskList from "../../components/taskList/taskList";
 
 export default function Dashboard() {
-  const [category, setCategory] = useState("head");
+	const [category, setCategory] = useState("head");
 
-  const customer = getCustomerInfo();
+	const customer = getCustomerInfo();
 
-  // const renderItem = () => {
-  const getItems = (category) => {
-    let content = [];
+	useEffect(() => {}, [customer.balance]);
 
-    for (let i = 0; i < 15; i++) {
-      content.push(<Item owned={true} image={`${category}/${i}`} />);
-    }
+	// const renderItem = () => {
+	const getItems = (category) => {
+		let content = [];
 
-    return content;
-  };
+		for (let i = 0; i < 15; i++) {
+			content.push(<Item owned={true} image={`${category}/${i}`} />);
+		}
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div className="content">
-        <div className="left">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              position: "relative",
-              marginBottom: 14,
-            }}
-          >
-            <h1
-              style={{
-                position: "absolute",
-                left: -500,
-                width: 400,
-              }}
-            >
-              {customer.firstName} {customer.lastName}
-            </h1>
-            <div style={{ display: "flex", marginTop: 38 }}>
-              <h4 style={{ margin: 0, marginRight: 5 }}>3200</h4>
-              <img src={Coin} height={20} width={20} />
-            </div>
-          </div>
-          <div className="panel1"></div>
-          <div className="panel2">
-            <div className="categories">
-              <a
-                onClick={() => setCategory("head")}
-                style={{ cursor: "pointer", marginRight: 25 }}
-              >
-                <p
-                  style={{
-                    marginTop: 0,
-                    fontWeight: category === "head" ? "bold" : "",
-                  }}
-                >
-                  Head
-                </p>
-              </a>
-              <a
-                onClick={() => setCategory("body")}
-                style={{ cursor: "pointer", marginRight: 25 }}
-              >
-                <p
-                  style={{
-                    marginTop: 0,
-                    fontWeight: category === "body" ? "bold" : "",
-                  }}
-                >
-                  Body
-                </p>
-              </a>
-              <a
-                onClick={() => setCategory("legs")}
-                style={{ cursor: "pointer", marginRight: 25 }}
-              >
-                <p
-                  style={{
-                    marginTop: 0,
-                    fontWeight: category === "legs" ? "bold" : "",
-                  }}
-                >
-                  Legs
-                </p>
-              </a>
-              <a
-                onClick={() => setCategory("feet")}
-                style={{ cursor: "pointer", marginRight: 25 }}
-              >
-                <p
-                  style={{
-                    marginTop: 0,
-                    fontWeight: category === "feet" ? "bold" : "",
-                  }}
-                >
-                  Feet
-                </p>
-              </a>
-              <a
-                onClick={() => setCategory("weapon")}
-                style={{ cursor: "pointer", marginRight: 25 }}
-              >
-                <p
-                  style={{
-                    marginTop: 0,
-                    fontWeight: category === "weapon" ? "bold" : "",
-                  }}
-                >
-                  Weapon
-                </p>
-              </a>
-            </div>
-            <div className="items">{getItems(category)}</div>
-          </div>
-        </div>
+		return content;
+	};
 
-        <div className="right">
-          <h1 style={{ marginBottom: 10, fontWeight: 100 }}>Tasks</h1>
-          <div className="panel3">
-            <TaskList />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div style={{ display: "flex", justifyContent: "center" }}>
+			<div className="content">
+				<div className="left">
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							position: "relative",
+							marginBottom: 14,
+						}}
+					>
+						<h1
+							style={{
+								position: "absolute",
+								left: -500,
+								width: 400,
+							}}
+						>
+							{customer.firstName} {customer.lastName}
+						</h1>
+						<div style={{ display: "flex", marginTop: 38 }}>
+							<h4 style={{ margin: 0, marginRight: 5 }}>
+								{customer.balance}
+							</h4>
+							<img src={Coin} height={20} width={20} />
+						</div>
+					</div>
+					<div className="panel1"></div>
+					<div className="panel2">
+						<div className="categories">
+							<a
+								onClick={() => setCategory("head")}
+								style={{ cursor: "pointer", marginRight: 25 }}
+							>
+								<p
+									style={{
+										marginTop: 0,
+										fontWeight:
+											category === "head" ? "bold" : "",
+									}}
+								>
+									Head
+								</p>
+							</a>
+							<a
+								onClick={() => setCategory("body")}
+								style={{ cursor: "pointer", marginRight: 25 }}
+							>
+								<p
+									style={{
+										marginTop: 0,
+										fontWeight:
+											category === "body" ? "bold" : "",
+									}}
+								>
+									Body
+								</p>
+							</a>
+							<a
+								onClick={() => setCategory("legs")}
+								style={{ cursor: "pointer", marginRight: 25 }}
+							>
+								<p
+									style={{
+										marginTop: 0,
+										fontWeight:
+											category === "legs" ? "bold" : "",
+									}}
+								>
+									Legs
+								</p>
+							</a>
+							<a
+								onClick={() => setCategory("feet")}
+								style={{ cursor: "pointer", marginRight: 25 }}
+							>
+								<p
+									style={{
+										marginTop: 0,
+										fontWeight:
+											category === "feet" ? "bold" : "",
+									}}
+								>
+									Feet
+								</p>
+							</a>
+							<a
+								onClick={() => setCategory("weapon")}
+								style={{ cursor: "pointer", marginRight: 25 }}
+							>
+								<p
+									style={{
+										marginTop: 0,
+										fontWeight:
+											category === "weapon" ? "bold" : "",
+									}}
+								>
+									Weapon
+								</p>
+							</a>
+						</div>
+						<div className="items">{getItems(category)}</div>
+					</div>
+				</div>
+
+				<div className="right">
+					<h1 style={{ marginBottom: 10, fontWeight: 100 }}>Tasks</h1>
+					<div className="panel3">
+						<TaskList />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- frontend/src/utils/backend.js
+++ frontend/src/utils/backend.js
@@ -1,74 +1,113 @@
 import axios from "axios";
 
-export const baseUrl = `https://cors-anywhere.herokuapp.com/http://fintavandy.herokuapp.com/`;
+export const baseUrl = `https://cors-anywhere.herokuapp.com/https://fintavandy.herokuapp.com/`;
 export const urlLogin = `${baseUrl}customer/`;
 export const taskUrl =
 	"https://cors-anywhere.herokuapp.com/https://17s5u775q5.execute-api.us-east-2.amazonaws.com/default/vandyHacks";
 
-export const postLogin = async (userName, password) => {
-  const config = {
-    method: "get",
-    url: `${urlLogin}${userName}`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  return axios(config)
-    .then((data) => {
-      if (data.status === 200) {
-        localStorage.setItem("id", data.data.id);
-        localStorage.setItem("email", data.data.email);
-        localStorage.setItem("firstName", data.data.firstName);
-        localStorage.setItem("lastName", data.data.lastName);
-        localStorage.setItem("balance", data.data.balance);
-        localStorage.setItem("items", data.data.items);
-        localStorage.setItem("username", data.data.username);
-      }
-      return true;
-    })
-    .catch((e) => {
-      return null;
-    });
+export const updateBalance = async (userName, amount) => {
+	const config = {
+		method: "put",
+		url: `${baseUrl}/customer/${userName}/updateBalance/${amount}`,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	console.log(`${userName} is getting ${amount} `);
+	console.log(config.url);
+
+	return axios(config)
+		.then((data) => {
+			let newBalance =
+				parseInt(localStorage.getItem("balance")) + parseInt(amount);
+			localStorage.setItem("balance", newBalance);
+			console.log(data);
+			window.location.reload();
+
+			return true;
+		})
+		.catch((e) => {
+			return false;
+		});
 };
 
+export const postLogin = async (userName, password) => {
+	const config = {
+		method: "get",
+		url: `${urlLogin}${userName}`,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return axios(config)
+		.then((data) => {
+			if (data.status === 200) {
+				const customer = {
+					id: data.data.id,
+					email: data.data.email,
+					firstName: data.data.firstName,
+					lastName: data.data.lastName,
+					balance: data.data.balance,
+					items: data.data.items,
+					username: data.data.username,
+				};
+				setLocalStorage(customer);
+			}
+			return true;
+		})
+		.catch((e) => {
+			return null;
+		});
+};
+
+function setLocalStorage(customer) {
+	localStorage.setItem("id", customer.id);
+	localStorage.setItem("email", customer.email);
+	localStorage.setItem("firstName", customer.firstName);
+	localStorage.setItem("lastName", customer.lastName);
+	localStorage.setItem("balance", customer.balance);
+	localStorage.setItem("items", customer.items);
+	localStorage.setItem("username", customer.username);
+}
+
 export function getCustomerInfo() {
-  if (localStorage.getItem("id")) {
-    let id = localStorage.getItem("id");
-    let email = localStorage.getItem("email");
-    let firstName = localStorage.getItem("firstName");
-    let lastName = localStorage.getItem("lastName");
-    let balance = localStorage.getItem("balance");
-    let items = localStorage.getItem("items");
-    let username = localStorage.getItem("username");
-    const customer = {
-      id: id,
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      balance: balance,
-      items: items,
-      username: username,
-    };
-    return customer;
-  } else return null;
+	if (localStorage.getItem("id")) {
+		let id = localStorage.getItem("id");
+		let email = localStorage.getItem("email");
+		let firstName = localStorage.getItem("firstName");
+		let lastName = localStorage.getItem("lastName");
+		let balance = localStorage.getItem("balance");
+		let items = localStorage.getItem("items");
+		let username = localStorage.getItem("username");
+		const customer = {
+			id: id,
+			email: email,
+			firstName: firstName,
+			lastName: lastName,
+			balance: balance,
+			items: items,
+			username: username,
+		};
+		return customer;
+	} else return null;
 }
 
 export function getTransactions() {
-  const config = {
-    method: "get",
-    url: `http://api.reimaginebanking.com/accounts/5f78b531f1bac107157e192e/purchases?key=7bdccafff961f37e0847de674d6f88be`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  return axios(config)
-    .then((data) => {
-      console.log(data[0]);
-      return data;
-    })
-    .catch((e) => {
-      return e;
-    });
+	const config = {
+		method: "get",
+		url: `http://api.reimaginebanking.com/accounts/5f78b531f1bac107157e192e/purchases?key=7bdccafff961f37e0847de674d6f88be`,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	return axios(config)
+		.then((data) => {
+			console.log(data[0]);
+			return data;
+		})
+		.catch((e) => {
+			return e;
+		});
 }
 
 export function getTasks() {
@@ -89,5 +128,5 @@ export function getTasks() {
 }
 
 export function isLoggedIn() {
-  return localStorage.getItem("id") != null;
+	return localStorage.getItem("id") != null;
 }
